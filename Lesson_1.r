c113e2a3ddc4cab9b60d376065ff042ff6db8edd
TakahiroYoshizawa <yoshizawatakahiro@yoshizawa-no-MacBook-Air.local>
2016-05-21 22:31:25 +0900

    よいしょ

--- Lesson_1.r
+++ Lesson_1.r
@@ -1,37 +1,40 @@
-Lesson1
 #データのインポートと確認・変更
-PassengerTS <- read.csv("airpass.csv") #読み込み
-PassengerTS #データの中身を全て表示　[ファイル名]
-head(Passenger) #上５行
-tail(Passenger) #下５行
-PassengerTS$Jan #毎年1月のデータを確認　[ファイル名$列名]
-PassengerTS[2,] 
-PassengerTS[,2] 
+dfPassenger <- read.csv("airpass.csv") #読み込み
+dfPassenger #データの中身を全て表示　[ファイル名]
+head(dfPassenger) #上５行
+tail(dfPassenger) #下５行
 
-colnames(PassengerTS)
-rownames(PassengerTS)
-rownames(PassengerTS) <- PassengerTS$X 
-
-PassengerTS[,-1]
+#扱いづらいので、１行目を消してからデータフレームから行列に変換
+dfPassenger <- dfPassenger[,-1]
+Passenger <- as.matrix(PassengerTS)
 
 #転置しましょう
-TPassengerTS <- t(PassengerTS)
+tPassenger <- t(Passenger)
 
 #平均を計算しましょう
-mean(TPassengerTS[1,])
-mean(TPassengerTS[,1])
+mean(Passenger[1,])
 
 #中央値を調べましょう
-median(TPassengerTS[1,])
-median(TPassengerTS[,1])
+median(Passenger[1,])
 
 #分散を計算しましょう
-var(TPassengerTS[,1])
+var(Passenger[1,])
 
 #標準偏差を計算しましょう
-sd(TPassengerTS[1,])
+sd(Passenger[1,])
+
+#すべての年について算出してみよう(for文の練習)
+for(i in 1:length(Passenger)){
+  n1 <- mean(Passenger[i,])
+  n2 <- median(Passenger[i,])
+  n3 <- var(Passenger[i,])
+  n4 <- sd(Passenger[i,])
+  print(c(n1,n2,n3,n4))
+}
+
 
 #プロットしましょう
-plot(TPassengerTS[1,])
-plot(TPassengerTS[,1])
+plot(Passenger[1,])
+plot(Passenger[1,],type='l')
 plot(AirPassengers)
+
